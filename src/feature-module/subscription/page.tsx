f641895ae4e0a117f20419a8a092a06c6aa64d66
Geektech <development.geektech@gmail.com>
2025-01-21 19:07:41 +0530

    Worked on Subscription List page

--- src/feature-module/subscription/page.tsx
+++ src/feature-module/subscription/page.tsx
@@ -93,7 +93,7 @@ export default function Subscription() {
 
     return (
         <div className="container py-4">
-          <div className="row justify-content-center">
+          <div className="row justify-content-center flex-column align-items-center">
           <ButtonGroup aria-label="Plan Selector" className="plan-selector">
           {plansList && plansList.map((plan:any)=>(<Button 
             variant={selectedPlan === plan.name ? "dark" : "outline-dark"}
@@ -107,31 +107,52 @@ export default function Subscription() {
           </Button>))}
           </ButtonGroup>
             {subscriptionList && subscriptionList.length > 0 && subscriptionList.map((item:any, index:number) => (
-              <div className="col-md-6 col-lg-4" key={index}>
+              <div className="planDetail col-md-6 col-lg-4" key={index}>
                 <div className="card shadow-sm rounded-4 border-0">
-                  <div className="card-body p-4">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                      <h5 className="text-primary">{item?.description}</h5>
-                      <h6 className="text-muted">{item?.price}</h6>
+                  <div className="card-body p-3">
+                    <div className="d-flex gap-3 align-items-center mb-3">
+                      {/* <h5 className="text-primary">{item?.description}</h5> */}
+                      <div className=''>
+                        <img src='/assets/img/paperPlane.png' alt='paperPlane' />
+                      </div>
+                      <div className='d-flex flex-column align-items-start'>
+                        <h5>Starter</h5>
+                        <h6 className="text-muted"><strong>${item?.price}</strong><span>/ Per month</span></h6>
+                      </div>
                     </div>
-                    <p className="fw-bold mb-4">Get access to</p>
+                    <p className="get">Get access to</p>
                     <ul className="list-unstyled">
-                      13,500+ gyms & studios,
-                    </ul>
-                    <ul>
-                      46 wellness apps,
-                    </ul>
-                    <ul>
-                      Live stream workouts,
-                    </ul>
-                    <ul>
-                      8x/month virtual personal training,
+                      <li><span><img src='/assets/img/location.svg' alt="location"/></span> 13,500+ gyms & studios,</li>
+                      <li><span><img src='/assets/img/phone_iphone.svg' alt="phone_iphone"/></span> 46 wellness apps,</li>
+                      <li><span><img src='/assets/img/exercise.svg' alt="exercise"/></span> Live stream workouts,</li>
+                      <li><span><img src='/assets/img/frame_person.svg' alt="frame_person"/></span> 8x/month virtual personal training,</li>
                     </ul>
                     <button type="button" className="custom-btn" onClick={()=>{
                       onPayment(item)
                     }}>
                       Go with this plan <span className="arrow">→</span>
                     </button>
+                    <div className='app-wrapper'>
+                    <p className="get">Apps</p>
+                    <ul>
+                      <li>
+                        <img src='/assets/img/fabulous.png' alt="location"/>
+                        <h5>Fabulous</h5>
+                      </li>
+                      <li>
+                        <img src='/assets/img/fabulous.png' alt="location"/>
+                        <h5>Fabulous</h5>
+                      </li>
+                      <li>
+                        <img src='/assets/img/fabulous.png' alt="location"/>
+                        <h5>Fabulous</h5>
+                      </li>
+                      <li>
+                        <img src='/assets/img/fabulous.png' alt="location"/>
+                        <h5>Fabulous</h5>
+                      </li>
+                    </ul>
+                    </div>
                     {loading && (
                       <span className='p-3'>
                         <Spinner animation="border" role="status">
